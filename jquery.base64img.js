a75f631d15be1b1c4b1d3b1243a8cd450e890a34
Faiz Shukri <faizshukri90@gmail.com>
2014-02-19 12:09:53 +0800

    Update javascript

--- jquery.base64img.js
+++ jquery.base64img.js
@@ -3,27 +3,32 @@
     $.fn.base64img = function( options ){
         var settings = $.extend({
             //list default here
-
+            result: '#result'
         }, options );
 
+        if(typeof(settings.url) == 'undefined'){
+            console.error('Url parameter not exist.');
+            return;
+        }
 
-        $('#image').on('change', function(e){
-                var url = URL.createObjectURL(e.target.files[0]),
-                    canvas = document.createElement('canvas'),
-                    ctx = canvas.getContext("2d"),
-                    img = new Image;
+        var url = URL.createObjectURL( settings.url ),
+            canvas = document.createElement('canvas'),
+            ctx = canvas.getContext("2d"),
+            img = new Image;
 
-                img.onload = function() {
-                    canvas.height = img.height;
-                    canvas.width = img.width;
-                    ctx.drawImage(img, 0, 0);
-                    var dataURL = canvas.toDataURL("image/jpeg");
-                    updateData(dataURL);
-                }
+        var updateData = function(dataURL){
+            $(settings.result).text(dataURL);
+        }
 
-                img.src = url;
-            });
+        img.onload = function() {
+            canvas.height = img.height;
+            canvas.width = img.width;
+            ctx.drawImage(img, 0, 0);
+            var dataURL = canvas.toDataURL("image/jpeg");
+            updateData(dataURL);
+        }
 
+        img.src = url;
         return this;
     }
 }( jQuery ));
