a51737341731654c25d753471c0ab384a440017f
Faiz Shukri <faizshukri90@gmail.com>
2014-02-19 11:52:20 +0800

    Replace tab indent with space

--- jquery.base64img.js
+++ jquery.base64img.js
@@ -1,11 +1,29 @@
 (function( $ ){
-	
-	$.fn.base64img = function( options ){
-		var settings = $.extend({
+
+    $.fn.base64img = function( options ){
+        var settings = $.extend({
             //list default here
 
         }, options );
 
-		return this;
-	}
+
+        $('#image').on('change', function(e){
+                var url = URL.createObjectURL(e.target.files[0]),
+                    canvas = document.createElement('canvas'),
+                    ctx = canvas.getContext("2d"),
+                    img = new Image;
+
+                img.onload = function() {
+                    canvas.height = img.height;
+                    canvas.width = img.width;
+                    ctx.drawImage(img, 0, 0);
+                    var dataURL = canvas.toDataURL("image/jpeg");
+                    updateData(dataURL);
+                }
+
+                img.src = url;
+            });
+
+        return this;
+    }
 }( jQuery ));
