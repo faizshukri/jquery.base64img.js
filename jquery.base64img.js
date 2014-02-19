0265a04666e2ad3d9cf3025ad79fff1f1389878b
Faiz Shukri <faizshukri90@gmail.com>
2014-02-19 10:12:47 +0800

    jQuery plugin bootstrap

---
+++ jquery.base64img.js
@@ -0,0 +1,11 @@
+(function( $ ){
+	
+	$.fn.base64img = function( options ){
+		var settings = $.extend({
+            //list default here
+
+        }, options );
+
+		return this;
+	}
+}( jQuery ));
